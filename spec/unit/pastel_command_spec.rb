e7ceec4ff1521d34eb080f812dcba77729833798
Piotr Murach <pmurach@gmail.com>
2016-01-08 23:57:25 +0100

    Change to simplify and test exit statuses.

--- spec/unit/pastel_command_spec.rb
+++ spec/unit/pastel_command_spec.rb
@@ -3,38 +3,32 @@
 RSpec.describe 'pastel command' do
 
   it 'runs without arguments and shows help' do
-    expect {
-      system('pastel')
-    }.to output(/PASTEL\(1\)/).to_stdout_from_any_process
+    expect(`pastel`).to match(/PASTEL\(1\)/)
+    expect($?.exitstatus).to eq(0)
   end
 
   it 'runs with text only' do
-    expect {
-      system('pastel foo')
-    }.to output(/foo/).to_stdout_from_any_process
+    expect(`pastel foo`).to match(/foo/)
+    expect($?.exitstatus).to eq(0)
   end
 
   it 'runs with foreground option' do
-    expect {
-      system('pastel green foo')
-    }.to output(/\e\[32mfoo\e\[0m/).to_stdout_from_any_process
+    expect(`pastel green foo`).to match(/\e\[32mfoo\e\[0m/)
+    expect($?.exitstatus).to eq(0)
   end
 
   it "runs with foreground & background options" do
-    expect {
-      system('pastel green on_red foo')
-    }.to output(/\e\[32;41mfoo\e\[0m/).to_stdout_from_any_process
+    expect(`pastel green on_red foo`).to match(/\e\[32;41mfoo\e\[0m/)
+    expect($?.exitstatus).to eq(0)
   end
 
   it "runs with foreground & background & style options" do
-    expect {
-      system('pastel green on_red bold foo')
-    }.to output(/\e\[32;41;1mfoo\e\[0m/).to_stdout_from_any_process
+    expect(`pastel green on_red bold foo`).to match(/\e\[32;41;1mfoo\e\[0m/)
+    expect($?.exitstatus).to eq(0)
   end
 
   it "runs with piped input" do
-    expect {
-      system('echo foo | pastel green')
-    }.to output(/\e\[32mfoo\e\[0m/).to_stdout_from_any_process
+    expect(`echo foo | pastel green`).to match(/\e\[32mfoo\e\[0m/)
+    expect($?.exitstatus).to eq(0)
   end
 end
