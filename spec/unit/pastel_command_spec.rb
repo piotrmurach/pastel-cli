2a0503d47373ca818437932f04fd031d88633082
Piotr Murach <pmurach@gmail.com>
2020-06-28 18:49:37 +0100

    Change to ensure colors always available

--- spec/unit/pastel_command_spec.rb
+++ spec/unit/pastel_command_spec.rb
@@ -1,44 +1,43 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 RSpec.describe 'pastel command' do
-
   it 'runs without arguments and shows help' do
-    expect(`bin/pastel`).to match(/PASTEL\(1\)/)
+    expect(`pastel`).to match(/PASTEL\(1\)/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "runs with --help/-h flags" do
-    expect(`bin/pastel`).to match(/PASTEL\(1\)/)
+    expect(`pastel`).to match(/PASTEL\(1\)/)
     expect($?.exitstatus).to eq(0)
   end
 
   it 'runs with text only' do
-    expect(`bin/pastel foo`).to match(/foo/)
+    expect(`pastel foo`).to match(/foo/)
     expect($?.exitstatus).to eq(0)
   end
 
   it 'runs with foreground option' do
-    expect(`bin/pastel green foo`).to match(/\e\[32mfoo\e\[0m/)
+    expect(`pastel green foo --enabled`).to match(/\e\[32mfoo\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "runs with foreground & background options" do
-    expect(`bin/pastel green on_red foo`).to match(/\e\[32;41mfoo\e\[0m/)
+    expect(`pastel --enabled green on_red foo`).to match(/\e\[32;41mfoo\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "runs with foreground & background & style options" do
-    expect(`bin/pastel green on_red bold foo`).to match(/\e\[32;41;1mfoo\e\[0m/)
+    expect(`pastel --enabled green on_red bold foo`).to match(/\e\[32;41;1mfoo\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "runs with piped input" do
-    expect(`echo foo | bin/pastel green`).to match(/\e\[32mfoo\e\[0m/)
+    expect(`echo foo | pastel green --enabled`).to match(/\e\[32mfoo\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "runs with --enabled option" do
-    expect(`bin/pastel --enabled green foo`).to match(/\e\[32mfoo\e\[0m/)
+    expect(`pastel --enabled green foo`).to match(/\e\[32mfoo\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 end
