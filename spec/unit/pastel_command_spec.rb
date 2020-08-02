afc4df1747767bcbe72d958606a621410c1776d2
Piotr Murach <pmurach@gmail.com>
2020-08-02 18:42:41 +0100

    Add -d option to specify delimiter character for separating content wrapped with styles

--- spec/unit/pastel_command_spec.rb
+++ spec/unit/pastel_command_spec.rb
@@ -9,9 +9,10 @@ RSpec.describe "pastel command" do
   it "runs with --help/-h flags" do
     out = <<-EOS
 Usage: pastel [options]
-    -f, --force                      Forces string coloring regardless whether terminal supports ANSI escape color sequences or not
+    -d delim                         Specifies character to use to split input coloring
+    -f, --force                      Forces string coloring regardless of terminal support
     -s, --styles                     Prints all available color and style names
-    -h, --help                       Display help
+    -h, --help                       Prints this message and exists
     EOS
     expect(`pastel --help`).to eq(out)
     expect($?.exitstatus).to eq(0)
@@ -20,6 +21,7 @@ Usage: pastel [options]
   it "handles invalid option without raising" do
     output = `pastel --unknown`
     expect(output).to eq("invalid option: --unknown\n")
+    expect($?.exitstatus).to eq(1)
   end
 
   it "runs with text only" do
@@ -67,5 +69,31 @@ Usage: pastel [options]
   it "shows available styles", unless: RSpec::Support::OS.windows? do
     output = `pastel --styles -f`
     expect(output).to match(/Swatch    Name\n\e\[1m◼ pastel\e\[0m  bold\n\e\[2m◼ pastel\e\[0m  dark/)
+  end
+
+  describe "split content on delim", unless: RSpec::Support::OS.windows? do
+    it "requires delim character" do
+      output = `pastel green foo -d`
+      expect(output).to eq("missing argument: -d\n")
+      expect($?.exitstatus).to eq(1)
+    end
+
+    it "splits on newline with delim separated by =" do
+      output = `printf 'foo\nbar\nbaz' | pastel green --force -d='\n'`
+      expect(output).to match(/\e\[32mfoo\e\[0m\n\e\[32mbar\e\[0m\n\e\[32mbaz\e\[0m/)
+      expect($?.exitstatus).to eq(0)
+    end
+
+    it "splits on newline with delim next to the option" do
+      output = `printf "foo\nbar\nbaz" | pastel green -fd'\n'`
+      expect(output).to match(/\e\[32mfoo\e\[0m\n\e\[32mbar\e\[0m\n\e\[32mbaz\e\[0m/)
+      expect($?.exitstatus).to eq(0)
+    end
+
+    it "splits on newline with -d option separate from delim character" do
+      output = `printf "foo\nbar\nbaz" | pastel green -f -d "\n"`
+      expect(output).to match(/\e\[32mfoo\e\[0m\n\e\[32mbar\e\[0m\n\e\[32mbaz\e\[0m/)
+      expect($?.exitstatus).to eq(0)
+    end
   end
 end
