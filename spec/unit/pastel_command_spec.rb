479aaf4fc09f26d27e8fa80034061d03578ce420
Piotr Murach <pmurach@gmail.com>
2020-08-02 16:41:29 +0100

    Change to use option parser and rename --enabled flag to --force/-f

--- spec/unit/pastel_command_spec.rb
+++ spec/unit/pastel_command_spec.rb
@@ -2,12 +2,18 @@
 
 RSpec.describe "pastel command" do
   it "runs without arguments and shows help" do
-    expect(`pastel`).to match(/PASTEL\(1\)/)
+    expect(`pastel`).to match(/Usage: pastel \[options\]/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "runs with --help/-h flags" do
-    expect(`pastel`).to match(/PASTEL\(1\)/)
+    out = <<-EOS
+Usage: pastel [options]
+    -f, --force                      Forces string coloring regardless whether terminal supports ANSI escape color sequences or not
+    -s, --styles                     Prints all available color and style names
+    -h, --help                       Display help
+    EOS
+    expect(`pastel --help`).to eq(out)
     expect($?.exitstatus).to eq(0)
   end
 
@@ -17,44 +23,44 @@ RSpec.describe "pastel command" do
   end
 
   it "runs with foreground option" do
-    expect(`pastel green foo --enabled`).to match(/\e\[32mfoo\e\[0m/)
+    expect(`pastel green foo --force`).to match(/\e\[32mfoo\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "runs with foreground & background options" do
-    expect(`pastel --enabled green on_red foo`).to match(/\e\[32;41mfoo\e\[0m/)
+    expect(`pastel -f green on_red foo`).to match(/\e\[32;41mfoo\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "runs with foreground & background & style options" do
-    expect(`pastel --enabled green on_red bold foo`).to match(/\e\[32;41;1mfoo\e\[0m/)
+    expect(`pastel -f green on_red bold foo`).to match(/\e\[32;41;1mfoo\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "runs with piped input" do
-    expect(`echo foo | pastel green --enabled`).to match(/\e\[32mfoo\s*\e\[0m/)
+    expect(`echo foo | pastel green -f`).to match(/\e\[32mfoo\s*\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 
-  it "runs with --enabled option" do
-    expect(`pastel --enabled green foo`).to match(/\e\[32mfoo\e\[0m/)
+  it "runs with --force option" do
+    expect(`pastel --force green foo`).to match(/\e\[32mfoo\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "preserves multiline content", unless: RSpec::Support::OS.windows? do
-    output = `echo "foo\nbar" | pastel green --enabled`
+    output = `echo "foo\nbar" | pastel green -f`
     expect(output).to match(/\e\[32m\s*foo\nbar\s*\e\[0m/)
     expect($?.exitstatus).to eq(0)
   end
 
   it "displays information when wrong styles are used" do
-    output = `pastel unknown foo --enabled`
+    output = `pastel unknown foo -f`
     expect(output).to match(/^Bad style or unintialized constant/)
     expect($?.exitstatus).to eq(1)
   end
 
   it "shows available styles", unless: RSpec::Support::OS.windows? do
-    output = `pastel --styles --enabled`
+    output = `pastel --styles -f`
     expect(output).to match(/Swatch    Name\n\e\[1m◼ pastel\e\[0m  bold\n\e\[2m◼ pastel\e\[0m  dark/)
   end
 end
